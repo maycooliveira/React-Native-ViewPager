da6c4ed8e3d14e17f44b89846902cf247d0f0acf
maycooliveira <maycoasamoda@hotmail.com>
2020-03-07 12:37:15 -0300

    Update ViewPager.js

--- viewpager/ViewPager.js
+++ viewpager/ViewPager.js
@@ -11,7 +11,7 @@ import {
   StyleSheet,
   View
 } from "react-native";
-import ViewPagerAndroid from "react-native-viewpager";
+import ViewPagerAndroid from "react-native-community/react-native-viewpager";
 import React, { Component } from "react";
 
 const SCROLLVIEW_REF = "scrollView";
